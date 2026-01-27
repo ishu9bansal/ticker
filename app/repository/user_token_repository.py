85744f12968bf7c656aeb11b5c46561c15928a61
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-27 18:45:13 +0530

    fix repo methods, commit db

--- app/repository/user_token_repository.py
+++ app/repository/user_token_repository.py
@@ -8,21 +8,27 @@ from .base import RepositoryBase
 class UserTokenRepository(RepositoryBase[UserToken]):
     def __init__(self, session: Session):
         super().__init__(session, UserToken)
-
-    def get_token(self, user_id: str) -> str | None:
+        
+    def _by_user_id(self, user_id: str) -> UserToken | None:
         stmt = select(UserToken).where(UserToken.user_id == user_id)
         result = self.session.execute(stmt).scalars().first()
+        return result
+
+    def get_token(self, user_id: str) -> str | None:
+        result = self._by_user_id(user_id)
         return result.token if result else None
 
+    def _update(self, user_token: UserToken) -> UserToken:
+        self.session.add(user_token)
+        self.session.commit()
+        self.session.refresh(user_token)
+        return user_token
+    
     def update_token(self, user_id: str, new_token: str) -> UserToken:
-        stmt = select(UserToken).where(UserToken.user_id == user_id)
-        user_token = self.session.execute(stmt).scalars().first()
-        if user_token:
-            user_token.token = new_token
-            self.add(user_token)
-            return user_token
+        obj = self._by_user_id(user_id)
+        if obj:
+            obj.token = new_token
+            return self._update(obj)
         else:
             new_user_token = UserToken(user_id=user_id, token=new_token)
-            self.add(new_user_token)
-            return new_user_token
-
+            return self._update(new_user_token)
